756dfa224fcc1f60ee11a4df4fe91469fc22ed28
clio19 <clio19@example.com>
2025-12-13 18:07:54 +0000

    Implement eslint and prettier

--- src/app.js
+++ src/app.js
@@ -1,15 +1,11 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-
 app.get('/', (req, res) => {
-//   logger.info('Hello from Acquisitions!');
-
+  //   logger.info('Hello from Acquisitions!');
   res.status(200).send('Hello from Acquisitions!');
 });
-
-
 
 app.use((req, res) => {
   res.status(404).json({ error: 'Route not found' });
