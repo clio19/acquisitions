e28f7379f7ff8153db5729c467d4ca18215fce00
clio19 <clio19@example.com>
2025-12-13 17:57:24 +0000

    created acquisitions

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+//   logger.info('Hello from Acquisitions!');
+
+  res.status(200).send('Hello from Acquisitions!');
+});
+
+
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
+});
+
+export default app;
